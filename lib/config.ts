f86067dcb38eacd6d02f77dc3e75cff07618f1e0
saurabh-nitj03 <sauravkrishnachauhan@gmail.com>
2025-07-06 13:05:44 +0530

    changes added

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 // import dotenv from "dotenv"
 // dotenv.config()
-export const BACKEND_URL=process.env.BACKEND_URL || "http://localhost:5000"
+export const BACKEND_URL=process.env.BACKEND_URL || "https://parlour-dashboard-83dz.onrender.com"
 if(!BACKEND_URL) throw new Error
 console.log(BACKEND_URL);
